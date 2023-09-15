724d39895cf634db9e6dfc514000a6dc9483e5c6
rajkashyap16 <78655730+rajkashyap16@users.noreply.github.com>
2023-09-16 00:19:36 +0530

    30 days projects

---
+++ app.js
@@ -0,0 +1,11 @@
+let div = document.querySelector("#div");
+function p(number, name) {
+  document.write(`
+    <li>
+    <a href="https://rajkashyap16.github.io/Day-${number}-challange/" target="iframe2">${number} . ${name}</a>
+    </li>
+
+    `);
+}
+
+
---
+++ function.js
@@ -0,0 +1,7 @@
+
+let button = document.querySelector('button')
+let ul = document.querySelector('.ul')
+
+button.addEventListener('click',()=>{
+  ul.classList.toggle('active')
+})
